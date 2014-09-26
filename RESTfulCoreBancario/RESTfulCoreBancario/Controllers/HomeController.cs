73bc84e093b65030e80d3ebcaeb813238766e8b3
dcanessa <danielcanessa09@gmail.com>
2014-09-25 20:08:34 -0600

    Se agrega el restful
    Se agrega coneccion con la base de datos

---
+++ RESTfulCoreBancario/RESTfulCoreBancario/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace RESTfulCoreBancario.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RESTfulCoreBancario/RESTfulCoreBancario/Services/CBConnectionMSQL.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace RESTfulCoreBancario.Services
+{
+    public class CBConnectionMSQL
+    {
+        public void insertUpdateDeleteMSQL(string query)
+        {
+            SqlConnection connection = new SqlConnection("user id=daniel;" +
+                                      "password=dcanessa;server=ENVY15-3040NR;" +
+                                      "Trusted_Connection=no;" +
+                                      "database=Envy; " +
+                                      "connection timeout=30");
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);                
+                command.ExecuteNonQuery();      
+
+            }
+            catch (Exception e)
+            {                
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+            finally
+            {
+                connection.Close();                
+            }
+        }
+
+        public DataTable retrieveMSQL(string query)
+        {
+            DataTable table = new DataTable();
+            SqlConnection connection = new SqlConnection("user id=daniel;" +
+                                       "password=dcanessa;server=ENVY15-3040NR;" +
+                                       "Trusted_Connection=no;" +
+                                       "database=Envy; " +
+                                       "connection timeout=30");
+            try
+            {
+                connection.Open();    
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();                
+                table.Load(reader);       
+            }
+            catch (Exception e)
+            {        
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+            finally
+            {
+                connection.Close();               
+            }
+            return table;
+
+        }
+    }
+}
+
+/*
+ foreach (DataRow row in table.Rows)
+ {
+    System.Diagnostics.Debug.WriteLine(row["ID"].ToString());
+    System.Diagnostics.Debug.WriteLine(row["Model"].ToString());
+ }
+*/
