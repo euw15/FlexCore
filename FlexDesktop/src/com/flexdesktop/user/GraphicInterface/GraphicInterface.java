33f4b680eb24cfb8dd0b44733944f3854d47cd29
Jason04 <jasago04@hotmail.com>
2014-09-27 17:49:37 -0600

    Intefaz
    algo de interfaz

---
+++ FlexDesktop/src/com/flexdesktop/user/GraphicInterface/GraphicInterface.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flexdesktop.user.GraphicInterface;
+
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Jason
+ */
+public class GraphicInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException |
+                IllegalAccessException | UnsupportedLookAndFeelException e) {
+        }
+
+        Frame a = new Frame();
+        a.setLocationRelativeTo(null);
+        a.setVisible(true);   
+    }
+
+}
--- FlexDesktop/src/flexdesktop/FlexDesktop.java
+++ FlexDesktop/src/flexdesktop/FlexDesktop.java
@@ -12,11 +12,11 @@ package flexdesktop;
  */
 public class FlexDesktop {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
     
 }
