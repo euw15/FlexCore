9ba96d6a5cd3e291003cf7b14fd952434ea76e69
dcanessa <danielcanessa09@gmail.com>
2014-09-28 20:05:55 -0600

    Add RestFul

---
+++ RESTfulFlexCore/RESTfulFlexCore/Services/CBConcreteFactoryWebServer .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTfulFlexCore.Services
+{
+    public class CBConcreteFactoryWebServer : CBAbstractFactoryWebServer
+    {
+        private static CBConcreteFactoryWebServer instanceCBConcreteFactoryWebServer;
+
+        private CBConcreteFactoryWebServer() {        
+        }
+
+        public static CBConcreteFactoryWebServer initInstanceCBConcreteFactoryWebServer
+        {
+            get
+            {
+                if (instanceCBConcreteFactoryWebServer == null)
+                {
+                    instanceCBConcreteFactoryWebServer = new CBConcreteFactoryWebServer();
+                }
+                return instanceCBConcreteFactoryWebServer;
+            }
+        }
+
+        public override CBClientAbstract CreateCBClientAbstract()
+        {
+            return new CBClientConcrete();
+        }
+
+
+    }
+}
