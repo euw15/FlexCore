984e1497ccd7c2f1d3da2471567584a77528def9
Edward <edwarduw@gmail.com>
2014-10-01 14:06:39 -0600

    Se agregaron algunos procedimientos almacenados

---
+++ CrearVistas.sql
@@ -0,0 +1,16 @@
+/********************Crea vistas *******************************************************/
+go
+CREATE VIEW ClientesFisicos
+AS select Cliente.CIF,ClienteFisico.Nombre,ClienteFisico.Cedula, Telefono.Telefono, Direccion.Direccion
+	from ClienteFisico 
+		INNER JOIN Cliente   on Cliente.CIF = ClienteFisico.CIF
+		INNER JOIN Telefono  on Telefono.idTelefono = ClienteFisico.idTelefonoPrincipal
+		INNER JOIN Direccion on Direccion.idDireccion = ClienteFisico.idDireccionPrincipal
+
+go
+CREATE VIEW ClientesJuridicos
+AS select Cliente.CIF,ClienteJuridico.Nombre,ClienteJuridico.Cedula, Telefono.Telefono, Direccion.Direccion
+	from ClienteJuridico 
+		INNER JOIN Cliente   on Cliente.CIF = ClienteFisico.CIF
+		INNER JOIN Telefono  on Telefono.idTelefono = ClienteJuridico.idTelefonoPrincipal
+		INNER JOIN Direccion on Direccion.idDireccion = ClienteJuridico.idDireccionPrincipal
