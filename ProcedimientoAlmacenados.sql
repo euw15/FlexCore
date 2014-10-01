57239409ea4923767c76c6ad6424928d6739b56a
Edward <edwarduw@gmail.com>
2014-10-01 15:46:53 -0600

    Se terminaron los procesos

--- ProcedimientoAlmacenados.sql
+++ ProcedimientoAlmacenados.sql
@@ -41,7 +41,6 @@ AS
 	INSERT INTO ClienteJuridico (CIF, Nombre,Cedula,idDireccionPrincipal,idTelefonoPrincipal) values (@CIFGenerado,@Nombre,@Cedula,@idDireccionGenerado,@idTelefonoGenerado);
 
 
-
 /**********************Crea un Empleado Fisico *******************/
 
 GO
@@ -79,7 +78,7 @@ AS
 	INSERT INTO ClienteFisico (CIF, Nombre,Apellidos,Cedula,idDireccionPrincipal,idTelefonoPrincipal) values (@CIFGenerado,@Nombre,@Apellidos,@Cedula,@idDireccionGenerado,@idTelefonoGenerado);
 
 
-/*******************Consultar CLientes por concepto******************************/
+/*******************Consultar CLientes Juridicos por concepto******************************/
 
 CREATE PROCEDURE consultarClienteJuridicoPorConcepto
 	/*Paramatros de entrada*/
@@ -87,7 +86,81 @@ CREATE PROCEDURE consultarClienteJuridicoPorConcepto
 	@Dato 	  nvarchar(200)
 AS
 	IF @Concepto = 'Nombre'
-		select 
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesJuridicos 
+		where ClientesJuridicos.Nombre = @Dato
+	IF @Concepto = 'Cedula'
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesJuridicos 
+		where ClientesJuridicos.Cedula = @Dato
+	IF @Concepto = 'Direccion'
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesJuridicos 
+		where ClientesJuridicos.Direccion = @Dato
+	IF @Concepto = 'Telefono'
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesJuridicos 
+		where ClientesJuridicos.Telefono = @Dato
+	IF @Concepto = 'CIF'
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesJuridicos 
+		where ClientesJuridicos.CIF = @Dato;
+
+/*******************Consultar CLientes Fisicos por concepto******************************/
+
+CREATE PROCEDURE consultarClienteFisicosPorConcepto
+	/*Paramatros de entrada*/
+	@Concepto nvarchar(200),
+	@Dato 	  nvarchar(200)
+AS
+	IF @Concepto = 'Nombre'
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesFisicos 
+		where ClientesFisicos.Nombre = @Dato
+	IF @Concepto = 'Cedula'
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesFisicos 
+		where ClientesFisicos.Cedula = @Dato
+	IF @Concepto = 'Direccion'
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesFisicos 
+		where ClientesFisicos.Direccion = @Dato
+	IF @Concepto = 'Telefono'
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesFisicos 
+		where ClientesFisicos.Telefono = @Dato
+	IF @Concepto = 'CIF'
+		select CIF,Nombre,Cedula, Telefono, Direccion
+		from ClientesFisicos 
+		where ClientesFisicos.CIF = @Dato;
+
+/*********************** ver Clientes   Fisico ******************************/
+
+go
+CREATE PROCEDURE consultarClientesFisicos
+	/*Paramatros de entrada*/
+	@Cantidad nvarchar(200),
+	@Inicio nvarchar(200)
+AS
+	SELECT CIF,Nombre,Cedula, Telefono, Direccion FROM ( 
+	  SELECT *, ROW_NUMBER() OVER (ORDER BY CIF) as row FROM ClientesFisico
+	 ) a WHERE a.row > @Inicio and a.row <= @Inicio+@Cantidad
+
+
+/*********************** ver Clientes   Juridico ******************************/
+go
+CREATE PROCEDURE consultarClientesJuridicos
+	/*Paramatros de entrada*/
+	@Cantidad nvarchar(200),
+	@Inicio nvarchar(200)
+AS
+	SELECT CIF,Nombre,Cedula, Telefono, Direccion FROM ( 
+	  SELECT *, ROW_NUMBER() OVER (ORDER BY CIF) as row FROM ClientesJuridicosView
+	 ) a WHERE a.row > @Inicio and a.row <= @Inicio+@Cantidad
+
+	
+
+
 
 
 
